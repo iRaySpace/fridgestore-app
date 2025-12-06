b0e0cf1f3707cabecfe8744a5a175fa413f0456c
irayspace <irayspacii@gmail.com>
2025-12-06 14:50:24 +0800

    refactor: use latest sqlalchemy syntax

--- backend/fridge_item/query.py
+++ backend/fridge_item/query.py
@@ -1,5 +1,6 @@
 import strawberry
 from typing import List
+from sqlalchemy import select
 from database.engine import transactional
 from .types import FridgeItemType
 from .models import FridgeItem
@@ -7,5 +8,6 @@ from .models import FridgeItem
 
 def get_fridge_items(limit: int = 10, offset: int = 0) -> List[FridgeItemType]:
     with transactional() as session:
-        data = session.query(FridgeItem).limit(limit).offset(offset).all()
+        stmt = select(FridgeItem).limit(limit).offset(offset)
+        data = session.execute(stmt).scalars().all()
         return [FridgeItemType.from_orm(d) for d in data]
