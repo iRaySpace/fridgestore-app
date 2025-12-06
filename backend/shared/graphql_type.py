15fee7c53192d441d801ea92b3746f48cfaefc55
irayspace <irayspacii@gmail.com>
2025-12-06 14:42:43 +0800

    feat: added UpdatedType

--- backend/shared/graphql_type.py
+++ backend/shared/graphql_type.py
@@ -1,9 +1,17 @@
 import strawberry
-from typing import Optional
+from typing import Optional, List, TypeVar, Generic
 
+
+T = TypeVar('T')
 
 @strawberry.type
 class DeletedType:
     success: bool
     message: str
     deleted_id: Optional[int] = None
+
+@strawberry.type
+class UpdatedType(Generic[T]):
+    success: bool
+    result: Optional[T] = None
+    errors: List[str] = strawberry.field(default_factory=list)
